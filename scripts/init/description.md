1f917ea754f2a3ad36d4faeaf6b2dd2a0dd1f761
saren <sahithnarahari@gmail.com>
2019-10-12 00:23:09 +0530

    modified script

--- scripts/init/description.md
+++ scripts/init/description.md
@@ -1,1 +1,4 @@
-This script ensures dependency and environment setup
+The script performs following steps needed for setting up 2 chains in a local system
+1) Dependency setup
+2) Environment setup
+3) Gaiacli configuration
--- scripts/init/ibc-local.sh
+++ scripts/init/ibc-local.sh
@@ -9,43 +9,77 @@ gaiacli version
 cd ~ && mkdir ibc-testnets && cd ibc-testnets
 gaiad testnet -o ibc0 --v 1 --chain-id ibc0 --node-dir-prefix n
 gaiad testnet -o ibc1 --v 1 --chain-id ibc1 --node-dir-prefix n
+
+#sed on OS X works different as it's based on FreeBSD
+#this configuration is to make different chains run on same system on different ports
 if [[ "$OSTYPE" == "darwin"* ]]; then
         echo "Sed done for osx"
   sed -i '' 's/"leveldb"/"goleveldb"/g' ibc0/n0/gaiad/config/config.toml
-sed -i '' 's/"leveldb"/"goleveldb"/g' ibc1/n0/gaiad/config/config.toml
-sed -i '' 's#"tcp://0.0.0.0:26656"#"tcp://0.0.0.0:26556"#g' ibc1/n0/gaiad/config/config.toml
-sed -i '' 's#"tcp://0.0.0.0:26657"#"tcp://0.0.0.0:26557"#g' ibc1/n0/gaiad/config/config.toml
-sed -i '' 's#"localhost:6060"#"localhost:6061"#g' ibc1/n0/gaiad/config/config.toml
-sed -i '' 's#"tcp://127.0.0.1:26658"#"tcp://127.0.0.1:26558"#g' ibc1/n0/gaiad/config/config.toml
+  sed -i '' 's/"leveldb"/"goleveldb"/g' ibc1/n0/gaiad/config/config.toml
+  sed -i '' 's#"tcp://0.0.0.0:26656"#"tcp://0.0.0.0:26556"#g' ibc1/n0/gaiad/config/config.toml
+  sed -i '' 's#"tcp://0.0.0.0:26657"#"tcp://0.0.0.0:26557"#g' ibc1/n0/gaiad/config/config.toml
+  sed -i '' 's#"localhost:6060"#"localhost:6061"#g' ibc1/n0/gaiad/config/config.toml
+  sed -i '' 's#"tcp://127.0.0.1:26658"#"tcp://127.0.0.1:26558"#g' ibc1/n0/gaiad/config/config.toml
 else
         echo "sed done for ubuntu"
   sed -i 's/"leveldb"/"goleveldb"/g' ibc0/n0/gaiad/config/config.toml
-sed -i 's/"leveldb"/"goleveldb"/g' ibc1/n0/gaiad/config/config.toml
-sed -i 's#"tcp://0.0.0.0:26656"#"tcp://0.0.0.0:26556"#g' ibc1/n0/gaiad/config/config.toml
-sed -i 's#"tcp://0.0.0.0:26657"#"tcp://0.0.0.0:26557"#g' ibc1/n0/gaiad/config/config.toml
-sed -i 's#"localhost:6060"#"localhost:6061"#g' ibc1/n0/gaiad/config/config.toml
-sed -i 's#"tcp://127.0.0.1:26658"#"tcp://127.0.0.1:26558"#g' ibc1/n0/gaiad/config/config.toml
+  sed -i 's/"leveldb"/"goleveldb"/g' ibc1/n0/gaiad/config/config.toml
+  sed -i 's#"tcp://0.0.0.0:26656"#"tcp://0.0.0.0:26556"#g' ibc1/n0/gaiad/config/config.toml
+  sed -i 's#"tcp://0.0.0.0:26657"#"tcp://0.0.0.0:26557"#g' ibc1/n0/gaiad/config/config.toml
+  sed -i 's#"localhost:6060"#"localhost:6061"#g' ibc1/n0/gaiad/config/config.toml
+  sed -i 's#"tcp://127.0.0.1:26658"#"tcp://127.0.0.1:26558"#g' ibc1/n0/gaiad/config/config.toml
 fi
 
 gaiacli config --home ibc0/n0/gaiacli/ chain-id ibc0
 gaiacli config --home ibc1/n0/gaiacli/ chain-id ibc1
 gaiacli config --home ibc0/n0/gaiacli/ node http://localhost:26657
 gaiacli config --home ibc1/n0/gaiacli/ node http://localhost:26557
+gaiacli config --home ibc0/n0/gaiacli/ output json
+gaiacli config --home ibc1/n0/gaiacli/ output json
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   echo "this is osx"
   brew install jq
-  jq -r '.secret' ibc1/n0/gaiacli/key_seed.json | pbcopy
 else
   echo "this is ubuntu"
   sudo apt install xclip -y
   sudo apt install jq
-  jq -r '.secret' ibc1/n0/gaiacli/key_seed.json | xclip -sel clip
-  gaiacli --home ibc0/n0/gaiacli keys add n1 --recover
-
 fi
 
-# Paste the mnemonic from the above command after setting password (12345678)
+#copy n0 key of ibc0
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "this is osx"
+    jq -r '.secret' ibc0/n0/gaiacli/key_seed.json | pbcopy
+else
+    echo "this is ubuntu"
+    jq -r '.secret' ibc0/n0/gaiacli/key_seed.json | xclip -sel clip
+fi
+
+# seed from ibc0/n0/gaiacli/key_seed.json -> ibc1/n0
+echo "the mnemonic is already copied to clipboard,just paste"
+gaiacli --home ibc1/n0/gaiacli keys add n0 --recover
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "this is osx"
+    jq -r '.secret' ibc1/n0/gaiacli/key_seed.json | pbcopy
+else
+    echo "this is ubuntu"
+    jq -r '.secret' ibc1/n0/gaiacli/key_seed.json | xclip -sel clip
+fi
+
+gaiacli --home ibc1/n0/gaiacli keys delete n0
+
+# seed from ibc1/n0/gaiacli/key_seed.json -> ibc0/n1
+echo "the mnemonic is already copied to clipboard,just paste"
 gaiacli --home ibc0/n0/gaiacli keys add n1 --recover
 
-echo "setup done,start two nodes now"
+# seed from ibc1/n0/gaiacli/key_seed.json -> ibc1/n1
+echo "the mnemonic is already copied to clipboard,just paste"
+gaiacli --home ibc1/n0/gaiacli keys add n1 --recover
+
+# Ensure keys match
+gaiacli --home ibc0/n0/gaiacli keys list | jq '.[].address'
+gaiacli --home ibc1/n0/gaiacli keys list | jq '.[].address'
+
+
+echo "gaiad started for both chains"
